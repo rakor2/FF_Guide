fb643cacac716cf1adb955bbe4d87fc9604d462a
rakor2 <winster2207@gmail.com>
2026-05-20 17:11:26 +0300

    Add Parallax DoF issue comparison

---
+++ image_compare/config-pd-issue.js
@@ -0,0 +1,9 @@
+const firstImage = "https://i.ibb.co/wZDMKVD4/Screenshot-3277b3-G9.jpg";
+const secondImage = "https://i.ibb.co/x83ZTHVZ/Screenshot-3278b5f3.jpg";
+const firstLabel = "I: 1 D: 1";
+const secondLabel = "I: 4 D: 4";
+
+document.getElementById("img-first").src = firstImage;
+document.getElementById("img-second").src = secondImage;
+document.getElementById("label-first").textContent = firstLabel;
+document.getElementById("label-second").textContent = secondLabel;
