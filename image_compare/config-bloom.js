c905142f71816f52b3bb8c72509d17c40fee54f1
rakor2 <winster2207@gmail.com>
2026-05-20 16:31:37 +0300

    Add bloom comparison

---
+++ image_compare/config-bloom.js
@@ -0,0 +1,9 @@
+const firstImage = "https://i.ibb.co/60FwscjR/bloom-1.webp";
+const secondImage = "https://i.ibb.co/SDy2rvdH/bloom-2.webp";
+const firstLabel = "In-game bs bloom";
+const secondLabel = "Based Xenon Bloom";
+
+document.getElementById("img-first").src = firstImage;
+document.getElementById("img-second").src = secondImage;
+document.getElementById("label-first").textContent = firstLabel;
+document.getElementById("label-second").textContent = secondLabel;
